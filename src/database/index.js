870bfa91600675cc7ae47349e7064727f235036d
Alisson <alisson.cs@hotmail.com>
2020-04-26 18:48:18 -0300

    Adicionando arquivo Procfile para o heroku e criando variavel de ambiente para a string de conexão com o mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv/config');
 
 // Criando conexão com o mongo
-mongoose.connect('mongodb://localhost/cadastroProdutos', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 .then(response=>console.log('Conected to Database..'))
 .catch(error=> console.log('error ->', error.message));
 
