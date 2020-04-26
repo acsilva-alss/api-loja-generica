890de8b6288d6cbe5d7825a2cb8ac42cf59a8c89
Alisson <alisson.cs@hotmail.com>
2020-04-26 19:01:18 -0300

    Adicionando varavel de ambiente para o nosso servidor rodar na porta que o heroku escolher

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+require('dotenv/config');
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
